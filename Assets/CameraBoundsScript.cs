e821b5367fb147111a732a559751dce2da78153e
kyle-li5 <126648458+kyle-li5@users.noreply.github.com>
2025-04-12 16:19:43 -0500

    Arena, camera, and upgrade scene changes
    Camera now follows bounds and switches between the tutorial and arena correctly. Changed the arena to be the correct size. Modified upgrade scene because its size was weird.

---
+++ Assets/CameraBoundsScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraBoundsScript : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D confinerShape2D; // Drag the zone’s collider here (self)
+    [SerializeField] GameObject player;
+    [SerializeField] Cinemachine.CinemachineVirtualCamera virtualCam;
+
+    //private void FixedUpdate()
+    //{
+    //    Debug.Log(Camera.main.name);
+    //    //Debug.Log(player.transform.position.y);
+    //    if (player.transform.position.y < -51f)
+    //    {
+    //        var camConfiner = virtualCam.GetComponentInChildren<Cinemachine.CinemachineConfiner2D>();
+    //        camConfiner.m_BoundingShape2D = confinerShape2D;
+    //        Debug.Log("Camera switched");
+    //    }
+    //}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // Make sure your player has the "Player" tag
+        {
+            var camConfiner = virtualCam.GetComponentInChildren<Cinemachine.CinemachineConfiner2D>();
+            camConfiner.m_BoundingShape2D = confinerShape2D;
+            Debug.Log("Camera switched");
+        }
+    }
+}
