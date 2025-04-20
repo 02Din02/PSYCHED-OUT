69888bc367fb05e050abdc06007c69cffc201d17
Bananapad987 <109549589+Bananapad987@users.noreply.github.com>
2025-04-19 19:55:09 -0500

    made tail start closer to boss and added slow

--- Assets/Scripts/Boss/BossTailController.cs
+++ Assets/Scripts/Boss/BossTailController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,26 +8,40 @@ public class BossTailController : MonoBehaviour
 
     private PlayerMovement player;
     private Rigidbody2D playerRB;
-
+    private float slow_percent = 0.3F;
+    private int exit_count = 0;
     void Awake()
     {
         player = FindObjectOfType<PlayerMovement>();
         playerRB = player.GetComponent<Rigidbody2D>();
-
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.tag == "Player" && exit_count == 0) 
+        {
+            player.Stats.BaseSpeed *= slow_percent;
+            Debug.Log("Slowed");
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
         if (collision.gameObject.tag == "Player") 
         {
-            PlayerManager script = player.GetComponent<PlayerManager>();
+            exit_count += 1;
+            StartCoroutine(delaySlow());
         }
     }
 
-    public IEnumerator slowPlayer()
+    // Wasn't sure what this did. Drag didn't seem to slow when I ran it at least. Could be wrong.
+    public IEnumerator delaySlow()
     {
-        playerRB.drag = 1;
         yield return new WaitForSeconds(3);
-        playerRB.drag = 0;
+        if (exit_count == 1){
+            player.Stats.BaseSpeed /= slow_percent;
+            
+            Debug.Log("Unslowed");
+        }
+        exit_count -= 1;
     }
 
 }
